ef394953cd9ba74513e71cacc3f17530e6dab22c
Paulo Mobile <bale_paulo@outlook.com>
2019-04-14 08:33:16 -0300

    Inserts de usuário

---
+++ AC06-Script_Insert_Lms.sql
@@ -0,0 +1,21 @@
+-- Inserts Usuário
+INSERT INTO Usuario (Login, Senha) VALUES(7656,'kose12')
+INSERT INTO Usuario (Login, Senha) VALUES(4543,'msdfd13')
+INSERT INTO Usuario (Login, Senha) VALUES(7864,'kmfgsk09')
+INSERT INTO Usuario (Login, Senha) VALUES(3455,'kjndfs4')
+INSERT INTO Usuario (Login, Senha) VALUES(1435,'fdxdx3')
+INSERT INTO Usuario (Login, Senha) VALUES(1456,'jkbl3')
+INSERT INTO Usuario (Login, Senha) VALUES(3456,'lblb3')
+INSERT INTO Usuario (Login, Senha) VALUES(4366,'kbççb4')
+INSERT INTO Usuario (Login, Senha) VALUES(6543,'blbh32')
+INSERT INTO Usuario (Login, Senha) VALUES(6534,'bllbh4')
+INSERT INTO Usuario (Login, Senha) VALUES(5764,'bkkkkk3')
+INSERT INTO Usuario (Login, Senha) VALUES(5475,'bkçj3')
+INSERT INTO Usuario (Login, Senha) VALUES(4743,'kjbç332')
+INSERT INTO Usuario (Login, Senha) VALUES(6523,'98y43n')
+INSERT INTO Usuario (Login, Senha) VALUES(4654,'7tnbjl')
+
+SELECT * FROM Usuario
+
+-- Inserts Coordenador
+
