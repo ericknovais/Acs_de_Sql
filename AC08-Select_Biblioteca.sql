f11c9a674813b90dcb28bffeaa6cfad3fcdaa576
ericknovais <erick.hora@Outlook.com>
2019-05-13 21:10:14 -0300

    AC08 Biblioteca

--- AC08-Select_Biblioteca.sql
+++ AC08-Select_Biblioteca.sql
@@ -1,16 +1,23 @@
-
 USE Biblioteca
-GO
--- Basico  
--- 1
-SELECT Nome from Livros WHERE Status = 'L'
-GO
--- 2
-SELECT Nome, Copia FROM Livros WHERE Status = 'L' 
-GO
--- 3
-Select * from Emprestimos emp
-join Clientes cli on cli.Id_Cliente = emp.ID_Cliente
-join Livros liv on emp.Copia = liv.Copia   
+GO 
+-- Basico -- 
+-- 1 --
+SELECT distinct nome from Livros 
 
-select * from Emprestimos
+-- 2--
+SELECT nome, copia from Livros where Status = 'livre' 
+
+-- 3 --
+SELECT CLI.Nome, LIV.Nome, LIV.copia, LIV.STATUS
+FROM Livros as LIV 
+JOIN Emprestimos as EMP on LIV.Nome = EMP.Livro AND LIV.Copia = EMP.Copia
+JOIN Clientes as CLI on EMP.ID_Cliente = CLI.Id_Cliente WHERE
+EMP.DataDevolucaoEfetiva IS NULL 
+
+-- 4 --
+SELECT Clientes.Nome from Clientes 
+JOIN Emprestimos ON Clientes.Id_Cliente = Emprestimos.ID_Cliente 
+WHERE Emprestimos.DataEmprestimo BETWEEN '2018-04-01' AND '2018-04-30' 
+
+-- 5 --
+SELECT * from Clientes where Telefone like '9%' 
