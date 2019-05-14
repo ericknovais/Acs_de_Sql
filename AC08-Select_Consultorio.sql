e780b86be09ebafed6400121be832261ac100605
Paulo Mobile <bale_paulo@outlook.com>
2019-05-13 22:18:58 -0300

    Selects básicos consultório

--- AC08-Select_Consultorio.sql
+++ AC08-Select_Consultorio.sql
@@ -1,8 +1,37 @@
 USE Consultorio
 GO
 
+-- BÃ¡sico -- 
+-- 1 --
+SELECT Nome,Telefone FROM Paciente 
 
--- Avançado --
+-- 2 -- 
+SELECT Numero_sala, DataHora 
+FROM Consulta
+INNER JOIN Medico
+ON Consulta.ID = Medico.ID
+WHERE Nome = 'Juvenal' ORDER BY DataHora
+
+-- 3 -- 
+SELECT * FROM Medico WHERE Especialidade = 'Dentista'
+
+-- 4 -- 
+SELECT Nome
+FROM Paciente
+INNER JOIN Consulta
+ON Paciente.ID = Consulta.ID_Paciente
+WHERE Consulta.DataHora BETWEEN CONVERT(DATE, '01/01/2017', 103) AND CONVERT(DATE, '31/12/2017', 103)
+
+-- 5 -- 
+SELECT M.Nome, P.Nome
+FROM Consulta 
+INNER JOIN Medico AS M
+ON Consulta.ID_Medico = M.ID
+INNER JOIN Paciente AS P
+ON Consulta.ID_Paciente = P.ID
+WHERE Duracao >= 60
+
+-- Avanï¿½ado --
 -- 1 --
 SELECT TOP 10 MED.Nome [Nome do Medico]
 FROM Consulta CON JOIN Medico MED
